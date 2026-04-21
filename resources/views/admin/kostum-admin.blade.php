294d31b4353884f5b5f05b01a713df69cb8e2565
Crist-afk <cristgarciapasaribu@gmail.com>
2026-04-21 09:24:11 +0700

    log out admin

--- resources/views/admin/kostum-admin.blade.php
+++ resources/views/admin/kostum-admin.blade.php
@@ -62,10 +62,13 @@
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><circle cx="12" cy="12" r="10"/><path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3"/><line x1="12" y1="17" x2="12.01" y2="17"/></svg>
         Bantuan &amp; Dukungan
       </a>
-      <a class="nav-item logout" href="{{ route('home') }}">
+      <a class="nav-item logout" href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"/><polyline points="16 17 21 12 16 7"/><line x1="21" y1="12" x2="9" y2="12"/></svg>
         Logout
       </a>
+      <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+        @csrf
+      </form>
     </div>
   </aside>
 
